5a1aa394b2ef315d1b7009b7fc27cb14378efab5
Charlie871001 <mahui871001@gmail.com>
2024-07-23 18:49:35 +0800

    form

---
+++ 10form/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 定义接收数据的结构体
+type Login struct {
+	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
+	User    string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
+	Pssword string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("loginForm", func(c *gin.Context) {
+		var form Login
+		if err := c.Bind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if form.User != "root" || form.Pssword != "admin" {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "200"})
+	})
+	r.Run(":8000")
+}
